1ea2bf87493d7919bcb471fa1ac8323071efacd5
Cedrick Oka Baidai <cedric.baidai@veone.net>
2024-08-28 13:13:07 +0000

    Updated constraints

--- src/Validator/ObjectExist.php
+++ src/Validator/ObjectExist.php
@@ -12,4 +12,20 @@ abstract class ObjectExist extends Constraint
     public $message = 'Object "%class%" with property "%property%": "%value%" does not exist.';
     public $property = 'id';
     public $class;
+
+    /**
+     * @see \Symfony\Component\Validator\Constraint::getDefaultOption()
+     */
+    public function getDefaultOption()
+    {
+        return 'class';
+    }
+
+    /**
+     * @see \Symfony\Component\Validator\Constraint::getRequiredOptions()
+     */
+    public function getRequiredOptions()
+    {
+        return ['class'];
+    }
 }
